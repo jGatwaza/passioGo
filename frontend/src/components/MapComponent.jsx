8df333eab92de554c765908d4d9fefa0495fad59
JEAN YVES GATWAZA <jgatwazakubwimana@college.harvard.edu>
2026-02-23 22:22:15 -0500

    Modified map

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -107,8 +107,9 @@ const MapComponent = ({
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        subdomains="abcd"
       />
 
       {shapes.map((shape) => (
